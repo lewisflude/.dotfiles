9ddf1163827037e1912e73d6c8e7f6f6998d2484
Lewis Flude <lewisflude@Lewiss-MacBook-Pro.local>
2019-11-14 14:21:27 +0000

    Use stow to manage dotfiles

--- Brewfile
+++ Brewfile
@@ -1,3 +1,4 @@
+brew "stow"
 brew "zsh"
 brew "tmux"
 brew "nvm"
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 dir=$(pwd -P)
-dotfiles=(vimrc zshrc tmux.conf)
+dotfiles=(alacritty zsh tmux vim)
 
 echo -e "\e[3mRunning bootstrap script\e[0m\n"
 
@@ -8,30 +8,50 @@ which -s brew
 if [[ $? != 0 ]] ; then
     echo "Installing brew"
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if (( $+commands[brew] )); then
+    echo "Brew already installed"
 else
-    echo "Updating brew"
-    brew update
+    echo "Installing brew"
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
 echo -e "\n\e[3mInstalling packages from brew\e[0m\n"
 brew bundle 
+echo -e "\n"
 
-echo "Installing zplugin"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zplugin/master/doc/install.sh)"
 
-echo "Installing vim-plug"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+if [ -d ~/.zplugin/bin ]; then
+    echo "Zplugin already installed" 
+else
+    echo "Installing zplugin"
+    mkdir ~/.zplugin
+    git clone https://github.com/zdharma/zplugin.git ~/.zplugin/bin
+fi
+
+if cat ~/.vim/autoload/plug.vim &> /dev/null; then
+    echo "Vim-plug already installed"
+else
+    echo "Installing vim-plug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-echo "Installing node"
-. ~/.nvm/nvm.sh
-nvm install node
+
+if (( $+commands[node] )); then
+    echo "Node already installed"
+else
+    echo "Installing node"
+    . ~/.nvm/nvm.sh
+    nvm install node
+fi
 
 echo -e "\n\e[3mAdding symlinks for dotfiles\e[0m\n"
 
 for file in $dotfiles; do
     echo "Symlinking $file"
-    ln -sf $dir/.$file ~/.$file
+    stow -t ~/ $file
 done
 
 echo "Symlinking alacritty.yml"
